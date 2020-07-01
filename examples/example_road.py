8e2a7bf4fea0ee5e9c411f0e3aa1bcd18d2c9321
hectortav <tavhector@gmail.com>
2020-07-01 23:59:51 +0300

    Access with python

--- examples/example_road.py
+++ examples/example_road.py
@@ -2,6 +2,12 @@ from ctypes import *
 import os
 
 akira_file = os.getcwd() + "/akira.so"
-print (akira_file)
 akira = CDLL(akira_file)
 print(type(akira))
+size = (c_int * 2)(2, 1)  # create an array of two ints
+network = akira.nn_constructor(1, size)
+training_input = akira.matrix_constructor(2, 1);
+training_output = akira.matrix_constructor(1, 1);
+
+for i in range(10):
+    #akira.train(network, training_input, training_output)
